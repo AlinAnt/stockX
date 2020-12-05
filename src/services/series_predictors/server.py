16908ea50ec7dd06482fa172b806e488ee3d4cb2
AlinAnt <antoniuk642@gmail.com>
2020-12-05 22:14:27 +0300

    init prediction server

---
+++ src/services/series_predictors/server.py
@@ -0,0 +1,36 @@
+import os
+from src.modules.db_helpers.config import CurrencyTables
+from src.modules.ts_models.runners import FrozenModel
+
+
+def poll():
+    print(currencies_pairs)
+
+
+if __name__ == '__main__':
+    global currencies_pairs
+    global models
+    currencies_pairs = {}
+    models = {}
+
+    # find pairs for currencies
+    for table in CurrencyTables:
+        name = table.name.split('_')
+        base_name = name[0]
+
+        if base_name not in currencies_pairs:
+            currencies_pairs[base_name] = {}
+
+        if len(name) == 1:
+            currencies_pairs[base_name]['original'] = table
+        else:
+            currencies_pairs[base_name]['prediction'] = table
+
+    # load models
+    for model_name in currencies_pairs.keys():
+        models[models] = FrozenModel(os.path.join('../../../models', f'{model_name}.ctb'))
+
+    print(models)
+
+    poll()
+    #print(CurrencyTables)
