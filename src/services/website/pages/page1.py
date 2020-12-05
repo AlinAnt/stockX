b593e102ef525d9ef3401b98e9a7ef2b36a5e66a
AlinAnt <antoniuk642@gmail.com>
2020-12-06 01:22:35 +0300

    data

--- src/services/website/pages/page1.py
+++ src/services/website/pages/page1.py
@@ -14,26 +14,36 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objs as go
 
+import sys
+sys.path.append("./../../../../stockx-release")
+from src.modules.db_helpers.helper import get_currency_data
+from src.modules.db_helpers.config import CurrencyTables
+
 login_alert = dbc.Alert(
     'User not logged in. Taking you to login.',
     color='danger'
 )
+def update_history_data():
+    global history_time, history_values
+    df = get_currency_data(CurrencyTables.BTC, hours_step=1)
+    last_date = df['unix_timestamp'][len(df) - 1]
+    week_interval = last_date - timedelta(weeks=30)
+    week_df = df[df['unix_timestamp'] > week_interval]
+    history_time = week_df['unix_timestamp']
+    history_values = week_df['close']
+    print(df)
 
-df = pd.read_parquet('data.parquet', engine="pyarrow")
-df["datetime"] = pd.to_datetime(df['datetime'])
-last_date = df['datetime'][len(df)-1]
-week_interval = last_date - timedelta(weeks=60)
-week_df = df[df['datetime'] > week_interval]
-week_df.iloc[::10080,:]
+def update_future_data():
+    global future_time, future_values
+    df = get_currency_data(CurrencyTables.BTC_PRED, hours_step=1)
+    future_time = df['unix_timestamp']
+    future_values = df['value']
+    
 
-dk = pd.read_csv("future.csv")
-dk["datetime"] = pd.to_datetime(dk['datetime'])
-first_date = dk['datetime'][0]
-our_interval = first_date + timedelta(weeks=60)
-our_dk = dk[dk['datetime'] < our_interval]
-
-fig = go.Figure([go.Scatter(x=week_df["datetime"], y=week_df['value'], name="Now", line=dict(color="#C70039")),
-                go.Scatter(x=our_dk['datetime'], y=our_dk['value'], name="Prediction", line=dict(color="#FFC300"))])
+update_history_data()
+update_future_data()
+fig = go.Figure([go.Scatter(x=history_time, y=history_values, name="Now", line=dict(color="#C70039")),
+                go.Scatter(x=future_time, y=future_values, name="Prediction", line=dict(color="#FFC300"))])
 fig.update_xaxes(
     rangeslider_visible=True,
 )
@@ -41,32 +51,7 @@ fig.update_layout(
     margin=dict(l=0, r=0, t=0, b=0),
     
 )
-today_value = int(df['value'][len(df)-1])
 
-#changes_currency_day
-yesterday_value = int(df["value"][df['datetime'] == (last_date - timedelta(days=1))])
-fig_change_day = go.Figure(go.Indicator(
-    mode = "number+delta",
-    value = today_value,
-    number = {'prefix':"$"},
-    delta = {'position': 'top', 'reference': yesterday_value},
-    domain = {'x': [0, 1], 'y': [0, 1]}
-
-))
-fig_change_day.update_layout(paper_bgcolor = "#d3e3f2")
-
-#changes_currency_week
-week_past_value =  int(df["value"][df['datetime'] == (last_date - timedelta(weeks=1))])
-fig_change_week = go.Figure(go.Indicator(
-    mode = "number+delta",
-    value = today_value,
-    number = {'prefix':"$"},
-    delta = {'position': 'top', 'reference': week_past_value},
-    domain = {'x': [0, 1], 'y': [0, 1]}
-
-))
-fig_change_week.update_layout(paper_bgcolor = "#d3e3f2")
-#fig_change_week.update_traces(title_text="Week", selector={type='indicator'})
 
 
 
@@ -85,16 +70,16 @@ def layout():
         
         dbc.Row([
             dbc.Col([
-                dcc.Graph(figure=fig_change_day)
+             #   dcc.Graph(figure=fig_change_day)
             ]),
             dbc.Col([
-                dcc.Graph(figure=fig_change_week)
+              #  dcc.Graph(figure=fig_change_week)
             ]),
             dbc.Col([
-                dcc.Graph(figure=fig_change_day)
+             #   dcc.Graph(figure=fig_change_day)
             ]),
             dbc.Col([
-                dcc.Graph(figure=fig_change_day)
+             #   dcc.Graph(figure=fig_change_day)
             ])
         ]),
 
